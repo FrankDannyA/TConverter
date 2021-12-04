1133114b96a68545042fb93dcd60800ae2c079f8
FrankDannyA <orgfrank@icloud.com>
2021-12-04 20:08:46 +0600

    Написал логику для Цельсия и Фаренгейта

--- TConverter/ViewController.swift
+++ TConverter/ViewController.swift
@@ -8,13 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var sliderValue = 0
 
     @IBOutlet weak var staticLabel: UILabel!
-    
     @IBOutlet weak var dinamicLabel: UILabel!
+    @IBOutlet weak var slider: UISlider!{
+        didSet{
+            slider.value = 0
+            slider.minimumValue = 0
+            slider.maximumValue = 100
+        }
+    }
     
-    @IBOutlet weak var slider: UISlider!
-    
-    @IBOutlet weak var sliderChanged: UISlider!
-}
+    @IBAction func sliderChanged(_ sender: UISlider) {
+        let temperatureCelsius = Int(round(sender.value))
+        let pharenheitTemperature = Int(round((sender.value + 9 / 5) + 32))
+        staticLabel.text = "\(temperatureCelsius)ºC"
+        dinamicLabel.text = "\(pharenheitTemperature)ºF"
+        }
+    }
 
